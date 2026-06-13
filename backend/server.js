da3a9c554b8ee2595875dcdcdf1cc002e9e94c71
Pogulaguntala Karthik Srinivas Varma <pk3188331@gmail.com>
2026-06-13 16:49:37 +0530

    chore: initialized backend folder and created server.js

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import exp from express
+const app= exp()
+
+// body Parser MiddleWare
+app.use(exp.json())
+// cookie Parser MiddleWare
+app.use()
+
+
+// function to connect to Database 
+function ConnectDb(){
+
+}
+
+
