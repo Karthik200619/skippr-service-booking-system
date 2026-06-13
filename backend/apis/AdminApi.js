9bf39ec59b40ca1da6902c552a9815f134c3c632
Pogulaguntala Karthik Srinivas Varma <pk3188331@gmail.com>
2026-06-13 19:24:42 +0530

    feat: initialize backend server and database connection

---
+++ backend/apis/AdminApi.js
@@ -0,0 +1,4 @@
+import exp from 'express'
+export const AdminApi = exp.Router()
+
+// 
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,39 @@
-import exp from express
-const app= exp()
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-// body Parser MiddleWare
-app.use(exp.json())
-// cookie Parser MiddleWare
-app.use()
+import { AdminApi } from "./apis/AdminApi.js";
+import { UserApi } from "./apis/UserApi.js";
+import { CommonApi } from "./apis/CommonApi.js";
+import sequelize from "./config/db.js";
 
+dotenv.config();
 
-// function to connect to Database 
-function ConnectDb(){
+const app = express();
 
-}
+// Middlewares
+// body parser middleware
+app.use(express.json());
+// cookie parser middleware
+app.use(cookieParser());
 
+// APIs
+app.use("/user-api", UserApi);
+app.use("/common-api", CommonApi);
+app.use("/admin-api", AdminApi);
 
+// Database Connection
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+
+    console.log("Database connection successful");
+    //  first connect to db then server start listening
+    app.listen(process.env.PORT, () => {console.log(`Server started on port ${process.env.PORT}`);});
+  } catch (err) {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  }
+};
+
+connectDB();
