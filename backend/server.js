7064155325229b6a12cf8f4d6f963d28d05463d1
Karthik200619 <23eg112e53@anurag.edu.in>
2026-06-14 17:36:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
  origin:[
   "http://localhost:5173",
-  "https://your-vercel-app.vercel.app"
+  "https://skippr-service-booking-system.vercel.app"
  ],
  credentials:true
 }));
